12ff531aa3a580c81b85a64ada1575a51082da98
Andra Gistescu <gistescu.andra@yahoo.com>
2020-03-10 16:28:49 +0200

    created route /schedule/year/:yearNumber

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,7 +1,24 @@
 const HttpStatus = require('http-status-codes')
+const fs = require('fs').promises
 
 exports.getRoomSchedule = async (req, res) => {
   try {
     return res.status(HttpStatus.OK).json(req.query.r)
   } catch (error) {}
+}
+
+exports.getYearSchedule = async (req, res) => {
+  try {
+    const { yearNumber } = req.params
+
+    let schedule = await fs.readFile('./data/schedule.json')
+    schedule = JSON.parse(schedule)
+
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      schedule: schedule[yearNumber]
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
