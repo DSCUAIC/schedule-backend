567b9d9593f23555f4738478e29a158e6bc5112c
Inga <ingaa.trifan@gmail.com>
2020-03-11 21:04:47 +0200

    new route /year/:yearNumber/semester/:semesterNumber

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,7 +1,25 @@
 const HttpStatus = require('http-status-codes')
+const fs = require('fs').promises
 
 exports.getRoomSchedule = async (req, res) => {
   try {
     return res.status(HttpStatus.OK).json(req.query.r)
   } catch (error) {}
+}
+
+exports.getYearSemesterSchedule = async (req, res) => {
+  try {
+    const  yearNumber  = req.params.yearNumber
+    const semesterNumber = req.params.semesterNumber;
+
+    let schedule = await fs.readFile(`./data/schedule${semesterNumber}.json`)
+    schedule = JSON.parse(schedule)
+
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      schedule: schedule[yearNumber]
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
--- routes/schedule.js
+++ routes/schedule.js
@@ -3,4 +3,6 @@ const { scheduleController } = require('../controllers')
 
 router.get('/rooms', scheduleController.getRoomSchedule)
 
+router.get('/year/:yearNumber/semester/:semesterNumber',scheduleController.getYearSemesterSchedule)
+
 module.exports = router
