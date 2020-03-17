3655b0c1aadcd06442e0b0d823dd887e8a3954b8
Alina <alina.pamfile@yahoo.com>
2020-03-17 13:12:31 +0400

    Improvements on login/register

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,11 +3,28 @@ const { createTkn } = require('../utils')
 
 exports.login = async (req, res) => {
   try {
-    const user = await req.db.User.findOne({ ...req.body })
+    const user = await req.db.User.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(HttpStatus.NOT_FOUND).json({
+        success: false,
+        message: 'User not found!'
+      })
+    }
+
+    if (req.body.password != user.password) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        success: false,
+        message: 'Incorrect password!'
+      })
+    }
+
     const token = createTkn(
       { ...user._doc, aud: req.config.TKN_AUD, iss: req.config.TKN_ISS },
       req.config.JWT_KEY
     )
+
+    delete user._doc.password
 
     return res.status(HttpStatus.OK).json({
       success: true,
@@ -23,7 +40,17 @@ exports.login = async (req, res) => {
 
 exports.register = async (req, res) => {
   try {
+    const existingUser = await req.db.User.findOne({ email: req.body.email })
+
+    if (existingUser) {
+      return res.status(HttpStatus.CONFLICT).json({
+        success: false,
+        message: "User already exists!"
+      })
+    }
+
     const user = await req.db.User.create(req.body)
+
     const token = createTkn(
       { ...user._doc, aud: req.config.TKN_AUD, iss: req.config.TKN_ISS },
       req.config.JWT_KEY
