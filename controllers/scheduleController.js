bacd8164473c0d3ef9e52b8d1ea6f3a512731f57
Radu Tache <radutache76@gmail.com>
2020-03-25 17:39:51 +0200

    Requested changes

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -16,12 +16,12 @@ exports.getRoomSchedule = async (req, res) => {
     for (const year in schedule) {
       for (const day in schedule[year]) {
         // create the empty object to add properties to
-        if (!Object.prototype.hasOwnProperty.call(result, year)) {
+        if (!result.hasOwnProperty(year)) {
           result[year] = {}
         }
 
         // creating the list of classes for that day
-        result[year][day] = Array.prototype.filter.call(schedule[year][day], entry => {
+        result[year][day] = schedule[year][day].filter(entry => {
           for (const room in rooms) {
             if (entry.Sala === rooms[room]) { return true }
           }
