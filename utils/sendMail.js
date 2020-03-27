a8f1842c3ea86473b80bf3caaa994cf263f93f07
RDaniel99 <rusudaniel5799@gmail.com>
2020-03-27 21:53:20 +0200

    solving npm lint issues part 2

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,20 +1,20 @@
 const sgMail = require('@sendgrid/mail')
 
 const templates = {
-    welcome: 'd-1f1ae50ff4584d169e15b6924caf07c8'
+  welcome: 'd-1f1ae50ff4584d169e15b6924caf07c8'
 }
 
-const sendEmail = function({config, to, template, vars}) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY || config.SENDGRID_API_KEY);
+const sendEmail = function ({ config, to, template, vars }) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY || config.SENDGRID_API_KEY)
 
-    const msg = {
-        to,
-        from: {name: "Support schedule app", email: "support@dsc-uaic.com"},
-        templateId: templates[template],
-        dynamicTemplateData: vars
-    };
+  const msg = {
+    to,
+    from: {name: 'Support schedule app', email: 'support@dsc-uaic.com'},
+    templateId: templates[template],
+    dynamicTemplateData: vars
+  }
 
-    return sgMail.send(msg);
+  return sgMail.send(msg)
 }
 
 module.exports = sendEmail
