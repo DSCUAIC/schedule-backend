a20ae6b76e268ab7a0f105330f298178d5cb9dd8
Victor Chiriac <victor.ldv@gmail.com>
2020-04-04 09:43:14 +0300

    GET admin/user route
    get users based on whether the current user is an admin or not

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const HttpStatus = require('http-status-codes')
+const { decodeTkn, getEvenToken, constants } = require('../utils')
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -13,6 +14,51 @@ exports.getAllUsers = async (req, res) => {
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
       success: false
     })
+  }
+}
+
+exports.getUsers = async (req, res) => {
+
+  //get the current user id from the token
+  const token = getEvenToken(req)
+  const decoded = decodeTkn(token, req.config.JWT_KEY)
+  const id = decoded[constants.idClaim]
+
+  //check if the current user is an admin
+  let isAdmin = req.user.admin
+  
+  if(!isAdmin) {
+    //get the data of the current user
+    try {
+      const userData = await req.db.User.findById(id)
+
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        userData
+      })
+    } catch (error) {
+      req.log.error(`Unable to get current user -> ${req.url} -> ${error}`)
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        success: false
+      })
+    }
+  }
+
+  if(isAdmin) {
+    //get the data of all users based on query parameters
+    try {
+      const users = await req.db.User.find(req.query)
+
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        users
+      })
+    } catch (error) {
+      req.log.error(`Unable to get users based on queries -> ${req.url} -> ${error}`)
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        success: false
+      })
+    }
   }
 }
 
