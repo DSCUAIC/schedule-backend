8d4fe23cdd76a9d0dc5124f263a996f89002d61e
andra5980 <56978400+andra5980@users.noreply.github.com>
2020-04-05 21:22:17 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,18 @@ exports.getAllUsers = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
   try {
+    const idToSearchFor = { _id: req.user['user:id'] }
+    const user = await req.db.User.findOne(idToSearchFor)
+
+    Object.keys(req.body).forEach( (key) => {
+      req.user[key] = req.body[key] 
+        })
+
+    await req.db.User.updateOne(user, req.body)
+
+    return res.json({
+        success: true
+    })
   } catch (error) {
     req.log.error(`Unable to update user -> ${error}`)
     return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
