cf90356494c44e56bc18c21710655417080ed8a1
Victor Chiriac <victor.ldv@gmail.com>
2020-04-05 21:26:37 +0300

    Update userController.js
    minor changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const HttpStatus = require('http-status-codes')
-const { decodeTkn, getEvenToken, constants } = require('../utils')
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -19,22 +18,18 @@ exports.getAllUsers = async (req, res) => {
 
 exports.getUsers = async (req, res) => {
 
-  //get the current user id from the token
-  const token = getEvenToken(req)
-  const decoded = decodeTkn(token, req.config.JWT_KEY)
-  const id = decoded[constants.idClaim]
-
   //check if the current user is an admin
   let isAdmin = req.user.admin
   
   if(!isAdmin) {
+    //the user is not an admin
     //get the data of the current user
     try {
-      const userData = await req.db.User.findById(id)
-
+      const user = await req.db.User.findById(req.user['user:id'])
+      
       return res.status(HttpStatus.OK).json({
         success: true,
-        userData
+        user
       })
     } catch (error) {
       req.log.error(`Unable to get current user -> ${req.url} -> ${error}`)
@@ -44,21 +39,20 @@ exports.getUsers = async (req, res) => {
     }
   }
 
-  if(isAdmin) {
-    //get the data of all users based on query parameters
-    try {
-      const users = await req.db.User.find(req.query)
+  //the user is an admin
+  //get the data of all users based on query parameters
+  try {
+    const users = await req.db.User.find(req.query)
 
-      return res.status(HttpStatus.OK).json({
-        success: true,
-        users
-      })
-    } catch (error) {
-      req.log.error(`Unable to get users based on queries -> ${req.url} -> ${error}`)
-      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false
-      })
-    }
+    return res.status(HttpStatus.OK).json({
+      success: true,
+      users
+    })
+  } catch (error) {
+    req.log.error(`Unable to get users based on queries -> ${req.url} -> ${error}`)
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false
+    })
   }
 }
 
