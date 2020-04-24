2678d27ddd340fff26b2ccffcf2a11e0759ae228
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-04-24 17:00:53 +0300

    Added favoriteCourses field to the model

--- models/user.js
+++ models/user.js
@@ -47,6 +47,11 @@ const userSchema = new Schema(
     admin: {
       type: Boolean,
       default: false
+    },
+    favoriteCourses:
+    {
+      type: Array,
+      default: []
     }
   },
   { timestamps: true }
