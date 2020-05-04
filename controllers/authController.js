805dba1ca876e00e57895e96ed386932aa178c31
Teodor Proca <cristian.cojocaru@thoughtspot.com>
2020-05-04 19:55:49 +0300

    fix validagte

--- controllers/authController.js
+++ controllers/authController.js
@@ -36,7 +36,7 @@ exports.login = async (req, res) => {
       })
     }
 
-    const token = createTkn({ [idClaim]: user._id }, req.config.JWT_KEY)
+    const token = createTkn({ _id: user._id }, req.config.JWT_KEY)
 
     return res.status(HttpStatus.OK).json({
       success: true,
@@ -65,7 +65,7 @@ exports.register = async (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, constants.saltRounds)
     const user = await req.db.User.create(req.body)
 
-    const token = createTkn({ [idClaim]: user._id }, req.config.JWT_KEY)
+    const token = createTkn({ _id: user._id }, req.config.JWT_KEY)
     const link = 'http://localhost:4200/auth/verification?token=' + token
     sendEmail({
       config: req.config,
@@ -89,7 +89,7 @@ exports.register = async (req, res) => {
 
 exports.validate = async (req, res) => {
   try {
-    const token = req.query.token
+    const { token } = req.body
     const claims = decodeTkn(token, req.config.JWT_KEY)
 
     const user = await req.db.User.findOne({ _id: ObjectId(claims[idClaim]) })
@@ -133,7 +133,7 @@ exports.forgotPassword = async (req, res) => {
 
     const token = createTkn(
       {
-        [idClaim]: user._id,
+        _id: user._id,
         [resetClaim]: true
       },
       req.config.JWT_KEY
