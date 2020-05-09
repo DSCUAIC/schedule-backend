2ea2cb34b6071432b3de88ded427d1cb9d98e255
Radu Ioan Tache <radutache76@gmail.com>
2020-05-09 15:00:42 +0300

    Replaced the requireAuth middleware

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const users = require('./users')
 const auth = require('./auth')
 const schedule = require('./schedule')
 const secrets = require('./secrets')
+const jwtAuth = require('jwt-auth-middleware')
+const Cryptr = require('cryptr')
+const db = require('../models')
 
 const { requireAuth } = require('../middlewares')
 
@@ -16,7 +19,12 @@ router.get('/', (req, res) => {
 
 router.use('/auth', auth)
 
-router.use(requireAuth())
+db.Secret.findOne({env: process.env.NODE_ENV, key: "JWT_KEY"}, (err, secret) => {
+
+  if(err) return handleError(err)
+  key = new Cryptr(process.env.SECRET_KEY).decrypt(secret.value)
+  router.use(jwtAuth.requireAuth(key))
+})
 
 router.use('/users', users)
 router.use('/schedule', schedule)
